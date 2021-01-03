caed051c6edb711325ff5a780a778b07c5306742
Ilia Choly <ilia.choly@gmail.com>
2021-01-03 15:10:31 -0500

    begin implementing the Ray type

---
+++ ray.go
@@ -0,0 +1,12 @@
+package raytrace
+
+// Ray is a ray of light
+type Ray struct {
+	Origin Vec3
+	Dir    Vec3
+}
+
+// Pos returns the ray position at t
+func (r Ray) Pos(t float64) Vec3 {
+	return r.Origin.Add(r.Dir.ScalarMul(t))
+}
